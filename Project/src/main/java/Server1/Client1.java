207fa7bf55be8b990d416bbd2e9d6696aee6f901
rafalW <s083670@student.tu.kielce.pl>
2020-04-17 21:48:13 +0200

    Simple Comunication Server-client.

---
+++ Project/src/main/java/Server1/Client1.java
@@ -0,0 +1,26 @@
+package Server1;
+
+import java.net.*;
+import java.io.*;
+class Client1{
+    public static void main(String args[])throws Exception{
+        Socket s=new Socket("localhost",2020);
+        DataInputStream din=new DataInputStream(s.getInputStream());
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String str="",str2="";
+        while(!str.equals("stop")){
+            str=br.readLine();
+            dout.writeUTF(str);
+            dout.flush();
+            str2=din.readUTF();
+            if(str.equals(str2)){
+                System.out.println("Przeciez juz to wiem ");
+            }
+            System.out.println("Server says: "+str2);
+        }
+
+        dout.close();
+        s.close();
+    }}
