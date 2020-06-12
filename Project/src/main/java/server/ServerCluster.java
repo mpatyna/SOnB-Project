b4ac4a6f92a185efa6ba4f16afd127ef477d05bd
Orlando3011 <34520084+Orlando3011@users.noreply.github.com>
2020-06-12 16:32:39 +0200

    Fixed bugs in gossip algorithm

--- Project/src/main/java/server/ServerCluster.java
+++ Project/src/main/java/server/ServerCluster.java
@@ -6,6 +6,7 @@ public class ServerCluster {
     private List<Server> servers;
     private List<String> consolMessages= new ArrayList<>();
     private Map<Integer, Integer> chart = new HashMap<>();
+    private int errorCounter;
 
     public ServerCluster() {
         this.servers = new ArrayList<>();
@@ -21,6 +22,7 @@ public class ServerCluster {
     }
 
     public List<String> gossip(String message) throws InterruptedException {
+        errorCounter = 0;
         consolMessages= new ArrayList<>();
         //dodaję wiadomość do pierwszego serwera
         servers.get(0).getMessages().add(message);
@@ -31,6 +33,7 @@ public class ServerCluster {
             consolMessages.add("----------------------------");
             consolMessages.add("Cykl nr. " + counter);
             List<Integer> destinations = new ArrayList<>();
+
             //znajduję serwery mające wiadomość do wysłania
             for (Server server:servers) {
                 if(server.getMessages().contains(message)) {
@@ -44,29 +47,53 @@ public class ServerCluster {
                 }
             }
             counter++;
-            //symuluję cykliczne rosyłanie
-            Thread.sleep(100);
+            if(errorCounter == 20) {
+                System.out.println("Osiągnięto limit błędów połączenia - część serwerów może być całkiem odłączona od sieci.");
+                System.out.println("Algorytm zajął: " + counter + " cykli");
+                break;
+            }
         }
-        consolMessages.add("Wszystkie serwery mają wiadomość. Zajęło to: " + counter + " cykli.");
+        if(errorCounter != 20) {
+            consolMessages.add("Wszystkie serwery mają wiadomość. Zajęło to: " + counter + " cykli.");
+        }
+        else {
+            System.out.println("Algorytm przerwany z powodu braku sprawnych połączeń. Ilość serwerów które mają wiadomość to: " + countServersWithMessage(message));
+        }
         chart.put(new Integer(chart.size()),new Integer(counter));
         return consolMessages;
     }
 
     public boolean ifAllServersHaveMessage(String message) {
         boolean allServersHasMessage = false;
-        int counter = 0;
-        for (Server server:servers) {
-            if (server.getMessages().contains(message)) counter++;
-        }
+        int counter = countServersWithMessage(message);
         if(counter == servers.size()) allServersHasMessage = true;
         consolMessages.add("Serwery mające wiadomość: " + counter);
         return  allServersHasMessage;
     }
 
+    private int countServersWithMessage(String message) {
+        int counter = 0;
+        for (Server server:servers) {
+            if (server.getMessages().contains(message)) counter++;
+        }
+        return  counter;
+    }
+
     public void send(Server from, String message) {
         Server destination;
-        //losuję serwer, aż nie znajdę niezablokowanego
+
+        //jeżeli serwer nie ma działających połączeń, zwiększam errorCount i idę do kolejnego serwera
+        int brokenConnections = 0;
         do {
+            for (Map.Entry<Integer, Float> entry: from.getAddresses().entrySet()) {
+                if(entry.getValue() <= 0) brokenConnections = brokenConnections + 1;
+            }
+            if(brokenConnections == 10) {
+                destination = new Server();
+                System.out.println("Nie można znaleźć działającego połączenia");
+                break;
+            }
+            //losuję serwer, aż nie znajdę niezablokowanego
             int i = 0;
             do {
                 Random rand = new Random();
@@ -77,27 +104,33 @@ public class ServerCluster {
         }
         while(from.getAddresses().get(servers.indexOf(destination)) == 0);
 
-        consolMessages.add("Wysyłam wiadomość z: " + servers.indexOf(from) + ", do: " + servers.indexOf(destination));
-        //obsługa zapsutego serwera
-        if(from.getAddresses().get(servers.indexOf(destination)) < 0) {
-            consolMessages.add("Połączenie uszkodzone, wysłanie nie powiodło się.");
-            float tmp = from.getAddresses().get(servers.indexOf(destination));
-            from.getAddresses().put(servers.indexOf(destination), tmp + 0.5F);
-            if(from.getAddresses().get(servers.indexOf(destination)) == 0)
-                consolMessages.add("Dwukrotnie serwer nie odebrał - nie będę więcej próbować");
-        }
-        if(destination.getMessages().contains(message)) {
-            //serwer docelowy ma już wiadomość
-            consolMessages.add("Serwer: " + servers.indexOf(destination) + " ma już tą wiadomość.");
-            float tmp = from.getAddresses().get(servers.indexOf(destination));
-            from.getAddresses().put(servers.indexOf(destination), tmp - 0.5F);
-            if(from.getAddresses().get(servers.indexOf(destination)) == 0)
-                consolMessages.add("Dwukrotnie serwer nie odebrał - nie będę więcej próbować");
+        if(destination.getAddresses().isEmpty()) {
+            errorCounter = errorCounter + 1;
+            System.out.println("Nie udało się znaleźć odpowiedniego adresata po raz: " + errorCounter);
         }
         else {
-            //wiadomość jest nowa
-            destination.getMessages().add(message);
-            consolMessages.add("Serwer: " + servers.indexOf(destination) + " odebrał wiadomość!");
+            consolMessages.add("Wysyłam wiadomość z: " + servers.indexOf(from) + ", do: " + servers.indexOf(destination));
+            //obsługa zapsutego serwera
+            if(from.getAddresses().get(servers.indexOf(destination)) < 0) {
+                consolMessages.add("Połączenie uszkodzone, wysłanie nie powiodło się.");
+                float tmp = from.getAddresses().get(servers.indexOf(destination));
+                from.getAddresses().put(servers.indexOf(destination), tmp + 0.5F);
+                if(from.getAddresses().get(servers.indexOf(destination)) == 0)
+                    consolMessages.add("Dwukrotnie serwer nie odebrał - nie będę więcej próbować");
+            }
+            else if(destination.getMessages().contains(message)) {
+                //serwer docelowy ma już wiadomość
+                consolMessages.add("Serwer: " + servers.indexOf(destination) + " ma już tą wiadomość.");
+                float tmp = from.getAddresses().get(servers.indexOf(destination));
+                from.getAddresses().put(servers.indexOf(destination), tmp - 0.5F);
+                if(from.getAddresses().get(servers.indexOf(destination)) == 0)
+                    consolMessages.add("Dwukrotnie serwer nie odebrał - nie będę więcej próbować");
+            }
+            else {
+                //wiadomość jest nowa
+                destination.getMessages().add(message);
+                consolMessages.add("Serwer: " + servers.indexOf(destination) + " odebrał wiadomość!");
+            }
         }
     }
 
